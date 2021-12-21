c4686c04370f3bdd9751b6e3f6e07342d22016f4
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-12-21 13:00:43 -0600

    Template URLs and Template inline with backticks

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  template: `<app-server></app-server>
+<!--  HTML Code goes here-->
+<!--  JS goes here-->
+<!--  Using backticks-->
+  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
